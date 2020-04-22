50ffdaa7bfba0146032b404ad0273d57838c4c89
EclipseAltair <eclipsealtair@yandex.ru>
2020-04-22 13:34:50 +0300

    add django.sites

--- dj_gulp/settings.py
+++ dj_gulp/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'main'
 ]
